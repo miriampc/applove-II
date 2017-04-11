c1d469a7393202095ab5f51822e9bec7a16df904
Miriam Peralta <miiriampc@gmail.com>
2017-04-11 14:32:35 -0500

    modal funcional

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,8 +1,6 @@
 window.addEventListener('load', function(){
   var container=document.getElementById('container');
-  var images=document.getElementsByClassName('box-work');
-  var modal=document.getElementsByClassName('modal');
-  var closeModal=document.getElementById('close-modal');
+  var docFragment=document.createDocumentFragment();
 
   allImages.forEach(function(e,id){
     var divBox=document.createElement('div');
@@ -10,37 +8,55 @@ window.addEventListener('load', function(){
     var image=document.createElement('img');
     var caption=document.createElement('figcaption');
     var h5Name=document.createElement('h5');
-    //modal
-    // var divModal=document.createElement('div');
-    // var modalContent=document.createElement('div');
 
     divBox.setAttribute("class",'box-work');
-    figure.setAttribute("class","image");
-    image.setAttribute("id","idImage"+id);
+    figure.setAttribute("class","figure");
     image.src="assets/images/img-"+(id+1)+".jpg";
     caption.setAttribute("class",'name-coder');
     h5Name.setAttribute("class",'mb-0');
+
     figure.appendChild(image);
     h5Name.appendChild(document.createTextNode(e));
     caption.appendChild(h5Name);
     figure.appendChild(caption);
     divBox.appendChild(figure);
     divBox.appendChild(h5Name);
-    container.appendChild(divBox);
-    //modal
-    // divModal.setAttribute("class",'modal');
-    // modalContent.setAttribute("class",'modal-content');
+    docFragment.appendChild(divBox);
+    container.appendChild(docFragment);
 
+    //eventos onclick
+    figure.onclick=function(e){
+      //crea modal invocando a funcion
+      divBox.appendChild(creaModal(image.src));
+
+      var modal=document.getElementById('modal');
+      var closeModal=document.getElementById('close-modal');
+
+      modal.classList.add("open");
+
+      closeModal.onclick=function(){
+        modal.classList.remove("open"); //quita display:block
+        divBox.removeChild(modal); //quita del dom todo el modal
+      }
+    };
   });
-
-  //eventos onclick
-  images[0].onclick=function(){
-    console.log(modal[0]);
-     modal[0].style.display="block";
-  };
-  closeModal.onclick=function(e){
-    console.log("aki");
-    modal[0].setAttribute("class","none");
-  };
-
 });
+
+function creaModal(src){
+  var divModal=document.createElement('div');
+  var modalContent=document.createElement('div');
+  var img=document.createElement('img');
+  var closeModal=document.createElement('span');
+
+  divModal.setAttribute("id",'modal');
+  divModal.setAttribute("class",'modal');
+  modalContent.setAttribute("class",'modal-content');
+  img.src=src;
+  closeModal.setAttribute("id",'close-modal');
+  closeModal.appendChild(document.createTextNode("X"));
+
+  modalContent.appendChild(img);
+  modalContent.appendChild(closeModal);
+  divModal.appendChild(modalContent);
+  return divModal;
+}
